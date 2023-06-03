48722c47137a7d876dc39725a5334b0eab262322
Sascha Endlicher, M.A <me@sascha.us>
2023-06-03 09:36:37 +0200

    Add support for searx
    Credits: @sammyf

--- script.py
+++ script.py
@@ -42,7 +42,8 @@ def ui():
     # This is a list of the inputs for the interface. These include a checkbox, a radio button, and several textboxes.
     components = [
         gr.inputs.Checkbox(label='SEARCH_ACCESS', default=True),  # A checkbox to control search access. The default value is True.
-        gr.inputs.Radio(['Google', 'DuckDuckGo'], label='SEARCH_ENGINE', default='Google'),  # A radio button to select the search engine. The default value is 'Google'.
+        gr.inputs.Radio(['Google', 'DuckDuckGo', 'Searx'], label='SEARCH_ENGINE', default='Searx'),  # A radio button to select the search engine. The default value is 'Google'.
+        gr.inputs.Textbox(label='SEARX_SERVER'),  # A textbox for the search query that will be submitted to Google.
         gr.inputs.Textbox(label='API_KEY'),  # A textbox for the Google API key.
         gr.inputs.Textbox(label='CSE_ID'),  # A textbox for the Google Custom Search Engine (CSE) ID.
         gr.inputs.Textbox(label='QUERY')  # A textbox for the search query that will be submitted to Google.
@@ -52,14 +53,46 @@ def ui():
 # This is a dictionary to hold parameters for search operation. Initial values are set here.
 params = {
     'SEARCH_ACCESS': True,
-    'SEARCH_ENGINE': 'Google',
+    'SEARCH_ENGINE': 'Searx',
+    'SEARX_SERVER' : 'http://localhost:80',
     'API_KEY': '',
     'CSE_ID': '',
-    'QUERY': 'Panomity GmbH'
+    'QUERY': 'Searx'
 }
 
-# This function is for processing the inputs based on the chosen search engine and query.
-def input_modifier(string):
+# This function is for calling the SEARX API and processing the output, credits: @sammyf
+def searx_api(string):
+    url=f"{params['SEARX_SERVER']}?q={string}&format=json"
+    response = requests.get(url)
+    # Load the response data into a JSON object.
+    data = json.loads(response.text)
+    # Initialize variables for the extracted texts and count of results.
+    texts = ''
+    count = 0
+    max_results = 3
+    result_max_characters = 250
+    # If there are items in the data, proceed with parsing the result.
+    if 'results' in data:
+        # For each result, fetch the webpage content, parse it, summarize it, and append it to the string.
+        for result in data['results']:
+            # Check if the number of processed results is less than or equal to the maximum number of results allowed.
+            if count <= max_results:
+                # Get the URL of the result.
+                link = result['url']
+                # Fetch the webpage content of the result.
+                content = result['content']
+                if len(content) > 0:  # ensure content is not empty
+                    # Append the summary to the previously extracted texts.
+                    texts = texts + ' ' + content
+                    # Increase the count of processed results.
+                    count += 1
+        # Add the first 'result_max_characters' characters of the extracted texts to the input string.
+        string += texts[:result_max_characters]
+    # Return the modified string.
+    return string
+
+# This function is for calling the Google API and processing the output
+def google_api(string):
     # If the selected search engine is Google, the search operation will be executed.
     if params["SEARCH_ENGINE"] == 'Google':
         # Construct the URL for Google's Custom Search JSON API.
@@ -103,6 +136,22 @@ def input_modifier(string):
             string += texts[:result_max_characters]
     # Return the modified string.        
     return string
+
+# this lists the processing functions specific to each search engine.
+# search_engine_processor = {
+#     'Google':google_api,
+#     'Searx':searx_api
+# }
+
+# This function is for processing the inputs based on the chosen search engine and query.
+def input_modifier(string):
+    rs = ""
+    if params["SEARCH_ENGINE"] == 'Google':
+        rs = google_api(string)
+    elif  params["SEARCH_ENGINE"] == 'Searx':
+        rs = searx_api(string)
+    return rs
+#    return search_engine_processor.get(params['SEARCH_ENGINE'])(string)
 
 # This function is to handle the event of parameter updates. It updates the global 'params' dictionary with the new input values.
 def update_params(inputs):
